559b01dec7e8793f4fd7209a68c1f7e2f3ecd409
Arnaud Baleh <33963757+arnaud355@users.noreply.github.com>
2021-04-22 19:16:09 +0200

    Add files via upload

--- flask/app.py
+++ flask/app.py
@@ -22,14 +22,37 @@ import time
 import pymongo
 import h5py
 import os
+import sentry_sdk
+import glob
+import gridfs
+import io
+import base64
+import PIL.Image as Image
+from array import array
+from bson import Regex
 
-#cd Desktop\dataIA\projetFilRouge\E1_projetFilRougeDl\flask  : python app.py
+from sentry_sdk.integrations.flask import FlaskIntegration
+#activate cifar, cd Desktop\dataIA\projetFilRouge\E1_projetFilRougeDl\flask  : python app.py
 app = Flask(__name__)
 #json = FlaskJSON(app)
 #api = Api(app)  # type: Api
 app.config['UPLOAD_FOLDER'] = 'static/result'
-app.config['DATA_DIR'] = '/Users/arnaud.baleh/Desktop/******/******/*******/flask/static/'
+app.config['DATA_DIR'] = '/Users/arnaud.baleh/Desktop/dataIA/projetFilRouge/E1_projetFilRougeDl/flask/static/'
 
+sentry_sdk.init(
+    dsn="https://examplePublicKey@o0.ingest.sentry.io/0",
+    integrations=[FlaskIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0
+)
+
+@app.route('/debug-sentry')
+def trigger_error():
+    division_by_zero = 1 / 0
+    
 @app.after_request
 def add_header(response):
     # response.cache_control.no_store = True
@@ -40,7 +63,15 @@ def add_header(response):
 
 @app.route("/")
 def accueil_page():
-    
+  
+    files = glob.glob('static/tmp/*.png', recursive=True)
+
+    for f in files:
+        try:
+            os.remove(f)
+        except OSError as e:
+            print("Error: %s : %s" % (f, e.strerror))
+             
     return render_template("accueil.html")
 
 @app.route("/handleTheme", methods=['POST'])
@@ -66,7 +97,7 @@ def handleThemePicture():
             # plot raw pixel data
             pyplot.imshow(examples[i, :, :])
         #pyplot.show()
-        plt.pyplot.imsave("/Users/*******/*****/*****/projetFilRouge/E1_projetFilRougeDl/flask/static/result/imageFantasy.png",examples[n])
+        plt.pyplot.imsave("/Users/arnaud.baleh/Desktop/dataIA/projetFilRouge/E1_projetFilRougeDl/flask/static/result/imageFantasy.png",examples[n])
     
     debut = time.time()
     
@@ -74,6 +105,7 @@ def handleThemePicture():
     mydb = myclient["datasetChefOeuvre"]
     mycol = mydb["model"]
     cursor = mycol.find()
+    myWeightsCol = mydb["weightsStorage"]
     
     json_data = ()
     for document in cursor:
@@ -99,19 +131,75 @@ def handleThemePicture():
     if valeur == "cartoon":
         
         #Load of weights
-        loaded_model.load_weights("modelWeightsCartoon.h5")
+        #loaded_model.load_weights(myWeightsCol.find({'weightsCartoon':'C:/Users/arnaud.baleh/Desktop/dataIA/projetFilRouge/E1_projetFilRougeDl/flask/modelWeightsCartoon.h5'}))
+        myArray = []
+        mydoc = myWeightsCol.find({'url':{'$regex':'Cartoon'}})
+        
+        for x in mydoc:
+            myArray.append(str(x))
+            
+        myString = str(myArray)
+        url = re.findall(r"C:/Users/arnaud.baleh/Desktop/dataIA/projetFilRouge/E1_projetFilRougeDl/flask/[a-zA-Z0-9]{10,50}.h5", myString)
+        url = str(url)
+        url = url[2:]
+        url = url[:-2]
+        loaded_model.load_weights(url)
         
     elif valeur == "videogaming":
-        loaded_model.load_weights("modelWeightsVideoGamingCharacters.h5")
+        myArray = []
+        mydoc = myWeightsCol.find({'url':{'$regex':'VideoGamingCharacters'}})
+        
+        for x in mydoc:
+            myArray.append(str(x))
+            
+        myString = str(myArray)
+        url = re.findall(r"C:/Users/arnaud.baleh/Desktop/dataIA/projetFilRouge/E1_projetFilRougeDl/flask/[a-zA-Z0-9]{10,50}.h5", myString)
+        url = str(url)
+        url = url[2:]
+        url = url[:-2]
+        loaded_model.load_weights(url)
     
     elif valeur == "heroic-fantasy":
-        loaded_model.load_weights("modelWeightsheroicFantasy.h5")
+        myArray = []
+        mydoc = myWeightsCol.find({'url':{'$regex':'heroicFantasy'}})
+        
+        for x in mydoc:
+            myArray.append(str(x))
+            
+        myString = str(myArray)
+        url = re.findall(r"C:/Users/arnaud.baleh/Desktop/dataIA/projetFilRouge/E1_projetFilRougeDl/flask/[a-zA-Z0-9]{10,50}.h5", myString)
+        url = str(url)
+        url = url[2:]
+        url = url[:-2]
+        loaded_model.load_weights(url)
     
     elif valeur == "heroes":
-        loaded_model.load_weights("modelWeightsSuperheroes.h5")
+        myArray = []
+        mydoc = myWeightsCol.find({'url':{'$regex':'Superheroes'}})
+        
+        for x in mydoc:
+            myArray.append(str(x))
+            
+        myString = str(myArray)
+        url = re.findall(r"C:/Users/arnaud.baleh/Desktop/dataIA/projetFilRouge/E1_projetFilRougeDl/flask/[a-zA-Z0-9]{10,50}.h5", myString)
+        url = str(url)
+        url = url[2:]
+        url = url[:-2]
+        loaded_model.load_weights(url)
     
     elif valeur == "saga":
-        loaded_model.load_weights("modelWeightsSagaIntergalactique.h5")
+        myArray = []
+        mydoc = myWeightsCol.find({'url':{'$regex':'SagaIntergalactique'}})
+        
+        for x in mydoc:
+            myArray.append(str(x))
+            
+        myString = str(myArray)
+        url = re.findall(r"C:/Users/arnaud.baleh/Desktop/dataIA/projetFilRouge/E1_projetFilRougeDl/flask/[a-zA-Z0-9]{10,50}.h5", myString)
+        url = str(url)
+        url = url[2:]
+        url = url[:-2]
+        loaded_model.load_weights(url)
     
     elif valeur == "":
         return "<h3>Vous n'avez pas sélectionné de thème!</h3>"
@@ -166,6 +254,49 @@ def view_picture():
 
     return render_template("rendu.html", image_color=full_filename,value=temps,choix=choix)
 
+#@route('/images/<filename>')
+@app.route("/handleImage", methods=['POST'])
+#def image(filename):
+def image():
+    choix = request.form['choix']
+    nombre = request.form['nombre']
+    
+    if choix == "":
+        choix = "file"
+        
+    if nombre == "":
+        nombre = 5
+    
+    nombre = int(nombre)
+    array = []
+    v = 0
+    for i in range(1,nombre + 1):
+        v = v + 1
+        w = v
+        w = str(w)
+        array.append(choix + w)
+ 
+        
+    #array = ["VideoGamingCharacters146","VideoGamingCharacters147","VideoGamingCharacters148","VideoGamingCharacters149","VideoGamingCharacters150"] 
+    # connect to database
+    connection = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = connection['DbDatasets']
+    fs = gridfs.GridFS(db)
+    
+    #filename = "VideoGamingCharacters146"
+    #gridout = fs.get_last_version(filename=filename)
+    
+    path = 'static/tmp/'
+    for ele in array:
+        data = fs.get_last_version(ele).read()
+        image = Image.open(io.BytesIO(data))
+        image.save(path + ele + ".png", "PNG")
+   
+    #response.content_type = 'image/png'
+    #return gridout
+  
 
+    return render_template("displayDataset.html",nb=nombre,tab=array)
+    
 if __name__ == "__main__":
     app.run(debug=True)
